ca44a8172e374dcde89542385410a9ddd9aac6b2
Pukeen2 <dziobalos@gmail.com>
2025-09-09 11:29:41 +0200

    Obliczanie punktow

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,31 @@ public class Main {
         Scanner scanner =  new Scanner(System.in);
         Random random = new Random();
         Boolean kontynuuj = true;
-        while(kontynuuj) {
+        int LiczbaKostek = 0;
+
+        do{
             System.out.println("Ile kostek chcesz rzucić? (3 - 10)");
             System.out.print(">>> ");
-            int LiczbaKostek = scanner.nextInt();
-            if (LiczbaKostek >= 3 && LiczbaKostek <= 10) {
-                Rzut rzut = new Rzut(LiczbaKostek);
-                rzut.WypiszWyniki();
+            LiczbaKostek = scanner.nextInt();
+        }while(LiczbaKostek < 3 || LiczbaKostek > 10);
+
+        while(kontynuuj) {
+            Rzut rzut = new Rzut(LiczbaKostek);
+            rzut.WypiszWyniki();
+
+            while(true){
                 System.out.println("Jeszcze raz? (t/n)");
                 String decision = scanner.next();
+
                 if(Objects.equals(decision, "n")){
                     kontynuuj = false;
+                    break;
+                }else if(Objects.equals(decision, "t")) {
+                    break;
                 }
+
             }
         }
+
     }
 }
